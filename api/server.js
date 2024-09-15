cf89e2574261ef06464a3ffc132f84adcbc817de
justinJSwebdev <trankyhung225@gmail.com>
2024-09-15 13:34:54 +0700

    chunk file using mern

--- api/server.js
+++ api/server.js
@@ -5,28 +5,29 @@ import fs from "fs";
 import md5 from "md5";
 
 const app = express();
-app.use(bodyParser.raw({type:'application/octet-stream', limit:'100mb'}));
+app.use(bodyParser.raw({ type: 'application/octet-stream', limit: '100mb' }));
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
 app.use('/uploads', express.static('uploads'));
 
 app.post('/upload', (req, res) => {
-  const {name,currentChunkIndex,totalChunks} = req.query;
+  const { name, currentChunkIndex, totalChunks } = req.query;
+  console.log('name-index' + name + "-" + currentChunkIndex)
   const firstChunk = parseInt(currentChunkIndex) === 0;
-  const lastChunk = parseInt(currentChunkIndex) === parseInt(totalChunks) -1;
+  const lastChunk = parseInt(currentChunkIndex) === parseInt(totalChunks) - 1;
   const ext = name.split('.').pop();
   const data = req.body.toString().split(',')[1];
   const buffer = new Buffer(data, 'base64');
   const tmpFilename = 'tmp_' + md5(name + req.ip) + '.' + ext;
-  if (firstChunk && fs.existsSync('./uploads/'+tmpFilename)) {
-    fs.unlinkSync('./uploads/'+tmpFilename);
+  if (firstChunk && fs.existsSync('./uploads/' + tmpFilename)) {
+    fs.unlinkSync('./uploads/' + tmpFilename);
   }
-  fs.appendFileSync('./uploads/'+tmpFilename, buffer);
+  fs.appendFileSync('./uploads/' + tmpFilename, buffer);
   if (lastChunk) {
     const finalFilename = md5(Date.now()).substr(0, 6) + '.' + ext;
-    fs.renameSync('./uploads/'+tmpFilename, './uploads/'+finalFilename);
-    res.json({finalFilename});
+    fs.renameSync('./uploads/' + tmpFilename, './uploads/' + finalFilename);
+    res.json({ finalFilename });
   } else {
     res.json('ok');
   }
